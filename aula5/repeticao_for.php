1a5742950efa7fcb9c0bbf7f202c3817ef09328c
Ivan Paulino <softip@gmail.com>
2023-03-08 10:37:09 -0300

    Repetição for

--- aula5/repeticao_for.php
+++ aula5/repeticao_for.php
@@ -1,3 +1,12 @@
+<style>
+  .grid{
+     display: grid;
+     grid-template-rows: auto auto;
+     grid-template-columns: auto auto auto auto auto;
+     gap: 10px;
+  }
+</style>
+
 <h2>Estrutura de repetição - for</h2>
 <p>
     Utilizada para repetir um conjunto de instruções por um
@@ -10,8 +19,17 @@
     }
 </pre>
 
-<?php
-    for($contador = 1; $contador < 10; $contador++){
-        echo "2 x $contador = <br>";
-    }
-?>
+
+
+<div class="grid">
+    <?php
+        for($tabuada = 1; $tabuada <= 10; $tabuada++){
+            echo "<div class='tabuada'>";
+                for($contador = 0; $contador <= 10; $contador++){
+                    $resultado = $tabuada * $contador;
+                    echo "$tabuada x $contador = $resultado<br>";
+                }
+            echo "</div>";
+        }
+    ?>
+</div>
