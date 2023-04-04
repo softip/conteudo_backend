a543ab9ac49071d55d6d79515374165750bcca4b
Ivan Paulino <softip@gmail.com>
2023-04-04 08:46:16 -0300

    aula 13

---
+++ aula12/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  </head>
+  <body>
+
+<!-- Carousel -->
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagem/foto1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imagem/foto2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imagem/foto3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- Final do Carousel -->
---
+++ aula12/ler.php
@@ -0,0 +1,18 @@
+<?php  include_once "dados.php" ?>
+<?php  include_once "header.php" ?>
+<?php  include_once "menu.php" ?>
+
+<?php  
+   $id = $_GET["id"];
+   $noticia = $noticias[$id];
+
+?>
+
+<div class="container">
+   <h1><?php echo $noticia['titulo']; ?></h1>
+   <hr>
+   <img src="<?php echo $noticia['foto']; ?>" class="" />
+   <p><?php echo $noticia['materia']; ?></p>
+</div>
+
+<?php  include_once "rodape.php" ?>
